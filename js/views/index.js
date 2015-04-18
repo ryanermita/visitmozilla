578729c4f053cb9578097cf9b7025789ef4a16aa
Ryan Jayson Ermita <ryanermita@gmail.com>
2015-04-18 10:52:45 +0800

    integrate persona login.

--- js/views/index.js
+++ js/views/index.js
@@ -2,16 +2,54 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/index.html'
-], function($, _, Backbone, indexTemplate){
+    'text!templates/index.html',
+    'persona'
+], function($, _, Backbone, indexTemplate, Persona){
   var indexView = Backbone.View.extend({
     el: $('.row'),
-
+    
     render: function(){
       compiledTemplate = _.template(indexTemplate);
       var data = {};
       this.$el.append(compiledTemplate, data);
+    },
+
+    events:{
+      'click #login-button': 'login',
+      'click #register-button': 'register'
+    },
+
+    // Use Callback Api of Mozilla Persona.
+    login: function(e){
+      navigator.id.get(this.verifyAssertion);
+      console.log('login button clicked!');
+    },
+
+    verifyAssertion: function(assertion){
+
+      // remove hashtag on the current url
+      var baseUrl = window.location.href.replace(/#/g, '');
+
+      $.ajax({
+        url: baseUrl + "functions/login.php",
+        method: "POST",
+        data: { assertion: assertion },
+        success: function(resp){
+          if(resp.success){
+            console.log('verify user success resp', resp);
+          } else {
+            console.log('verify user failed resp', resp);
+          }
+        }
+      });
+
+      console.log('assertion');
+    },
+
+    register: function(){
+      console.log('register me ohhh');
     }
+
     
   });
 
